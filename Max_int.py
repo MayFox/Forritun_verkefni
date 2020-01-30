d235f03f950f650f062bff981601500c6fdfc648
Erna <ernaomarsdottir96@gmail.com>
2020-01-30 16:19:44 +0000

    Update Max_int.py

--- Max_int.py
+++ Max_int.py
@@ -3,30 +3,28 @@ south_int = int(input("Number of cars travelling south: "))
 east_int = int(input("Number of cars travelling east: "))
 west_int = int(input("Number of cars travelling west: "))
 
-W_e = west_int + east_int
-#cars = 1
-N_s = south_int + north_int
-while north_int > 0 or south_int > 0 or west_int > 0 or east_int > 0: 
-    #cars = south_int + north_int + west_int + east_int
-    if north_int or south_int > west_int or east_int:
+
+while north_int > 0 or south_int > 0 or west_int > 0 or east_int > 0:
+    if north_int > east_int and north_int > west_int or south_int > east_int and south_int > west_int:
         if north_int > 0:
             north_int = north_int - 5
-        elif north_int <= 0:
-            north_int = 0
-        elif south_int > 0: 
+        if south_int > 0:
             south_int = south_int - 5
-        elif south_int <= 0:
-            south_int
-        print("Green light on N/S")
-    elif N_s < W_e: 
+        print ("Green light on N/S")
+    else:
+        if east_int > 0:
+            east_int = east_int - 5
         if west_int > 0:
             west_int = west_int - 5
-        elif west_int <= 0:
-            west_int = 0
-        elif east_int > 0: 
-            east_int = east_int - 5
-        elif east_int <= 0:
-            east_int
-        print("Green light on E/W")
+        print ("Green light on E/W")
 
-print("No cars waiting, the traffic jam has been solved!")S
+    if north_int < 0:
+        north_int = 0
+    if south_int < 0:
+        south_int = 0
+    if west_int < 0:
+        west_int = 0
+    if east_int < 0:
+        east_int = 0
+
+print("No cars waiting, the traffic jam has been solved!")
